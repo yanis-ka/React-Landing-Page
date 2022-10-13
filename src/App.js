0191f6448a213fd5b82ced0857c7f9d36ab8eb88
yanis khellaf <yanis.khellaf@outlook.fr>
2022-10-13 03:56:17 +0200

    files structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+import {  CTA, Brand, Navbar} from './components'
 
 function App() {
   return (
-    <div>
-      Landing page
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
